fdaecf7952f653610c9921562acba8a2c361d39a
FacuTello <facundostello@hotmail.com>
2025-07-20 16:59:28 -0300

    Se corrige error en el footer

--- ProyectoFinal_FT/src/App.jsx
+++ ProyectoFinal_FT/src/App.jsx
@@ -52,6 +52,7 @@ function App() {
               }
             />
           </Routes>
+          <Footer/>
         </Router>
       </CarritoProvider>
     </AuthProvider>
---
+++ ProyectoFinal_FT/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer className="mt-5 py-3 bg-primary text-white" data-bs-theme="dark">
+      <Container className="text-center">
+        Desarrollado por Facundo Tello para TalentoTech
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
